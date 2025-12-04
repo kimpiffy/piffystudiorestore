d54b3fde103e5d358006ed0184b9c691712ecadc
Kim Piffy <kimpiffy@Kims-iMac.local>
2025-12-04 20:55:12 +0000

    setting up database

--- config/settings.py
+++ config/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import dj_database_url
 from dotenv import load_dotenv
 
 
@@ -96,13 +97,13 @@ TEMPLATES = [
 WSGI_APPLICATION = 'config.wsgi.application'
 
 # -------------------------------
-# DATABASE (SQLite for now)
+# DATABASE (PostgreSQL)
 # -------------------------------
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")
+    )
 }
 
 # -------------------------------
